319d9e130ca9602276ec3c2dbc5f7b017fc29ec4
Justin Vorster <87868083+JustVorster@users.noreply.github.com>
2021-10-23 17:41:45 +0200

    Uploaded Part 1
    Uploaded code for BitCube Internship Project Part 1.

---
+++ BitCube Part1/stocks.js
@@ -0,0 +1,6 @@
+/*
+I wasn't able to build the needed functionality for the stock management system.
+
+I had written some script but it was giving errors when trying to display in the Stock table.
+
+*/
